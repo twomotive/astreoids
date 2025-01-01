0679bec13f09a0b8ea2f1cfe52f3a733d888b5c8
twomotive <112391727+twomotive@users.noreply.github.com>
2025-01-01 16:46:06 +0300

    completed basic mechanics

---
+++ circleshape.py
@@ -0,0 +1,30 @@
+import dis
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+    
+    def collides_with(self,object):
+        distance = self.position.distance_to(object.position)
+        
+        if distance <= self.radius + object.radius:
+            return True
+        return False
