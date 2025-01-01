c915ef1b22cfa8da1ac1a5e489a1f1e240d92bcb
twomotive <112391727+twomotive@users.noreply.github.com>
2025-01-01 12:48:13 +0300

    add pygame clock and delta time for smoother game loop control

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0,0,0))
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
     
     
